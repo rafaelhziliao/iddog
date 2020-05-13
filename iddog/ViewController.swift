9063804723a9abc4f2cc9cbfb713ef86e07297f5
Rafael Ziliao <rafael.ziliao@dextra-sw.com>
2020-05-13 11:24:05 -0300

    Initial Commit

---
+++ iddog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iddog
+//
+//  Created by Rafael Henrique Zilião on 13/05/20.
+//  Copyright © 2020 Rafael Henrique Zilião. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
