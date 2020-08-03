6317f424c15bdb044db49be02fa821d6c0092d6b
Rafael Ziliao <rafael.ziliao@dextra-sw.com>
2020-08-03 11:40:56 -0300

    Add tests

--- iddogTests/Scenes/Login/LoginPresenterTests.swift
+++ iddogTests/Scenes/Login/LoginPresenterTests.swift
@@ -26,27 +26,55 @@ class LoginPresenterTests: XCTestCase {
     // MARK: Test doubles
 
     class LoginDisplayLogicSpy: LoginDisplayLogic {
-        func displayCategories(with user: UserProtocol) {
+        var displayCategoriesCalled = false
+        var displaySignUpErrorCalled = false
 
+        func displayCategories(with user: UserProtocol) {
+            displayCategoriesCalled = true
         }
 
         func displaySignUpError(_ error: NetworkError) {
-
+            displaySignUpErrorCalled = true
         }
     }
 
     // MARK: Tests
 
-    func testPresentSomething() {
+    func testPresentCategories() {
         // Given
         let spy = LoginDisplayLogicSpy()
         sut.viewController = spy
 
+        let user = UserModel(
+            email: "teste",
+            id: "1",
+            token: "token",
+            createdAt: "createdAt",
+            updatedAt: "createdAt",
+            version: 1
+        )
 
         // When
-        
+        sut.presentCategories(user: user)
 
         // Then
-        //    XCTAssertTrue(spy.displaySomethingCalled, "presentSomething(response:) should ask the view controller to display the result")
+        XCTAssertTrue(
+            spy.displayCategoriesCalled,
+            "presentSomething(response:) should ask the view controller to display the result"
+        )
+    }
+
+    func testPresentSignUpError() {
+        let spy = LoginDisplayLogicSpy()
+        sut.viewController = spy
+
+        // When
+        sut.presentSignUpError(.unauthorized)
+
+        // Then
+        XCTAssertTrue(
+            spy.displaySignUpErrorCalled,
+            "presentSomething(response:) should ask the view controller to display the result"
+        )
     }
 }
--- iddogTests/Scenes/Login/LoginWorkerTests.swift
+++ iddogTests/Scenes/Login/LoginWorkerTests.swift
@@ -1,1 +1,122 @@
+@testable import iddog
+import XCTest
 
+class LoginWorkerTests: XCTestCase {
+    // MARK: Subject under test
+
+    var sut: LoginWorker!
+
+    // MARK: Test lifecycle
+
+    override func setUp() {
+        super.setUp()
+        setupLoginWorker()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    // MARK: Test setup
+
+    func setupLoginWorker() {
+        sut = LoginWorker(
+            network: NetworkProviderSpy(),
+            credentialStorage: CrendentialsStorageSpy()
+        )
+    }
+
+    // MARK: Test doubles
+
+    class SecurityServiceSpy: SecurityServiceProtocol {
+        func save(_ value: String, forKey key: String) {}
+
+        func get(_ key: String) -> String? {
+            return nil
+        }
+
+        func clear() -> Bool {
+            return false
+        }
+    }
+
+    class CrendentialsStorageSpy: SecurityStorageProtocol {
+        var service: SecurityServiceProtocol
+        var setAccessTokenAsyncCalled =  false
+
+        func getAccessTokenSync() -> String? {
+            return nil
+        }
+
+        func setAccessTokenAsync(_ token: String?) {
+            setAccessTokenAsyncCalled = true
+        }
+
+        func clear() {}
+
+        init(service: SecurityServiceProtocol = SecurityServiceSpy()) {
+            self.service = service
+        }
+    }
+
+    class NetworkProviderSpy: NetworkProviderProtocol {
+        var performRequestCalled = false
+
+        func performRequest<T: Decodable>(endpoint: EndpointDescriptor, result: @escaping ResultHandler<T>) {
+            performRequestCalled = true
+        }
+    }
+
+    // MARK: Tests
+
+    func testWorkerAskToCredentialStorageToStoreAccessToken() {
+        // Given
+        let token = "token"
+
+        //swiftlint:disable:next force_cast
+        let crendentialsStorageSpy = sut.credentialStorage as! CrendentialsStorageSpy
+
+        // When
+        sut.storeAccessToken(token)
+
+        // Then
+        XCTAssertTrue(
+            crendentialsStorageSpy.setAccessTokenAsyncCalled,
+            "Calling storeAccessToken(token) should ask to credentialsStorage to store token"
+        )
+    }
+
+    func testWorkerAskToNetworkProviderToSignUp() {
+        // Given
+        let email = "teste@teste.com"
+
+        //swiftlint:disable:next force_cast
+        let networkProviderSpy = sut.network as! NetworkProviderSpy
+
+        // When
+        sut.signUp(with: email) { _ in }
+
+        // Then
+        XCTAssertTrue(
+            networkProviderSpy.performRequestCalled,
+            "Calling sut.signUp(with: email) should ask to network to signUp"
+        )
+    }
+
+    func testWorkerNotAskToNetworkProviderToSignUp() {
+        // Given
+        let email: String? = nil
+
+        //swiftlint:disable:next force_cast
+        let networkProviderSpy = sut.network as! NetworkProviderSpy
+
+        // When
+        sut.signUp(with: email) { _ in }
+
+        // Then
+        XCTAssertFalse(
+            networkProviderSpy.performRequestCalled,
+            "Calling sut.signUp(with: email) should not ask to network to signUp"
+        )
+    }
+}
